295f86120ae6853cfaccb7d0d5ecbfbf8276ebbe
BButner <butner.beau@gmail.com>
2022-04-19 16:41:17 -0500

    Remove useless code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
 import {NextPage} from "next";
-import {Window} from "../components/window/Window";
 import {useEffect, useRef} from "react";
 import {useStore} from "../lib/state/state";
-import {motion} from "framer-motion";
 import {Finder} from "../components/window/windows/Finder";
 
 const Index: NextPage = () => {
   const desktopRef = useRef(null);
-  const windows = [<Window key={1}>
-    <div className="bg-purple-500">
-      <p className="p-8">This is a test with me typing in here</p>
-    </div>
-  </Window>];
 
   useEffect(() => {
     if (desktopRef) useStore.setState({desktop: desktopRef});
