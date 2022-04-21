34e9473ca652575cabb86469bb05c014d7cc38c7
BButner <butner.beau@gmail.com>
2022-04-21 15:14:35 -0500

    Persist window location

--- components/window/Window.tsx
+++ components/window/Window.tsx
@@ -1,10 +1,8 @@
-import {DragControls, motion, useDragControls} from "framer-motion";
+import {DragControls, motion} from "framer-motion";
 import {AdditionalClassNames, ChildrenProps} from "../../lib/props";
-import styles from "./Window.module.sass";
 import {useStore} from "../../lib/state/state";
 import clsx from "clsx";
-import {WindowButtons} from "./WindowButtons";
-import {RefObject, useEffect, useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export interface WindowProps {
   windowId: string;
@@ -42,15 +40,23 @@ const WindowComponent: React.FC<ChildrenProps & AdditionalClassNames & DragContr
     const windowRef = useRef(null);
     const [top, setTop] = useState(0);
     const [left, setLeft] = useState(0);
+    const [translateX, setTranslateX] = useState('');
+    const [translateY, setTranslateY] = useState('');
 
     useEffect(() => {
       if (store.desktop && windowRef.current && top === 0 && left === 0) {
+        console.log(top, left, windowId)
         const desktop = (store.desktop.current as Element);
         const window = (windowRef.current as Element);
-        setTop((desktop.clientHeight / 2) - (window.clientHeight / 2));
-        setLeft((desktop.clientWidth / 2) - (window.clientWidth / 2));
+        const centerTop = (desktop.clientHeight / 2) - (window.clientHeight / 2);
+        const centerLeft = (desktop.clientWidth / 2) - (window.clientWidth / 2);
+        const savedX = localStorage.getItem(`${windowId}X`);
+        const savedY = localStorage.getItem(`${windowId}Y`);
+
+        setTop(centerTop + Number(savedY));
+        setLeft(centerLeft + Number(savedX));
       }
-    }, [store.desktop, store.activeWindowId]);
+    }, [store.desktop]);
 
     if (!store.desktop) return <></>
 
@@ -71,8 +77,23 @@ const WindowComponent: React.FC<ChildrenProps & AdditionalClassNames & DragContr
         }}
         onDragEnd={() => {
           document.documentElement.classList.remove('noSelect');
+          if (windowRef.current) {
+            const w = (windowRef.current as Element);
+            const matrix = new WebKitCSSMatrix(window.getComputedStyle(w).transform);
+
+            const prevX = localStorage.getItem(`${windowId}X`);
+            const prevY = localStorage.getItem(`${windowId}Y`);
+
+            if (prevX && prevY) {
+              localStorage.setItem(`${windowId}X`, (matrix.m41 + Number(prevX)).toString());
+              localStorage.setItem(`${windowId}Y`, (matrix.m42 + Number(prevY)).toString());
+            } else {
+              localStorage.setItem(`${windowId}X`, matrix.m41.toString());
+              localStorage.setItem(`${windowId}Y`, matrix.m42.toString());
+            }
+          }
         }}
-        style={{top, left}}
+        style={{top, left, transform: `translate(${translateX}px,${translateY}px)`}}
         className={clsx(
           'rounded-lg',
           'text-zinc-900 dark:text-white transition-colors transition-opacity duration-200',
