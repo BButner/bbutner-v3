9e592de1c3c3dba01a1e987a4c5ce315ac7a5903
BButner <butner.beau@gmail.com>
2022-04-21 16:00:50 -0500

    Dock improvements

--- components/layout/Dock.tsx
+++ components/layout/Dock.tsx
@@ -1,8 +1,9 @@
 import {useStore} from "../../lib/state/state";
 import Image from "next/image";
-import {OnClickButton} from "../../lib/props";
+import {AdditionalClassNames, OnClickButton} from "../../lib/props";
 import {Finder} from "../window/windows/Finder";
 import styles from "./Dock.module.sass";
+import clsx from "clsx";
 
 export const Dock: React.FC = () => {
   const store = useStore();
@@ -22,6 +23,8 @@ export const Dock: React.FC = () => {
       <DockIcon imageSrc={"/images/dock_icons/finder.png"} key="finder" onClick={openFinder}/>
       <div className={styles.divider}/>
       {useStore.getState().openWindows.filter(window => window.windowId !== 'finder').map((window, index) => <DockIcon imageSrc={window.iconHref} key={index}/>)}
+      <div className={styles.divider}/>
+      <DockIcon imageSrc={"/images/dock_icons/trash.png"} key={"trash"} transparent/>
     </div>
   )
 }
@@ -29,10 +32,11 @@ export const Dock: React.FC = () => {
 interface DockIconProps {
   imageSrc: string;
   key: string | number;
+  transparent?: boolean;
 }
 
-const DockIcon: React.FC<DockIconProps & OnClickButton> = ({imageSrc, key, onClick}) => {
-  return <button onClick={onClick} className="m-0 p-0 w-8 h-8 bg-zinc-800 rounded-md">
-    <Image key={key} src={imageSrc} height={32} width={32} className="rounded-md"/>
+const DockIcon: React.FC<DockIconProps & OnClickButton> = ({imageSrc, key, onClick, transparent}) => {
+  return <button onClick={onClick} className={clsx(styles.dockButton, !transparent ? 'bg-zinc-700' : '')}>
+    <Image key={key} src={imageSrc} width={32} height={32}/>
   </button>
 }
