6ada13c530e6a5398e953eddca85e5ffad02b923
BButner <butner.beau@gmail.com>
2022-04-25 12:57:10 -0500

    Fix wallpaper in dark mode

--- components/layout/Wallpaper.tsx
+++ components/layout/Wallpaper.tsx
@@ -21,6 +21,10 @@ export const Wallpaper: React.FC = () => {
     <div className={clsx(
       'absolute inset-0 transition-opacity duration-200',
       show ? 'opacity-1' : 'opacity-0'
-    )} style={{backgroundImage: 'url("/images/' + state.wallpaper.fileNameDark + '")'}}/>
+    )} style={{
+      backgroundImage: 'url("/images/' + state.wallpaper.fileNameDark + '")',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }}/>
   )
 }
