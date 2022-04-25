76bfbe5874caba6d47e9aabb2d94c33e30b88910
BButner <butner.beau@gmail.com>
2022-04-25 10:29:44 -0500

    Fix control center icon?

--- components/layout/buttons/ControlCenter.tsx
+++ components/layout/buttons/ControlCenter.tsx
@@ -1,13 +1,35 @@
 import {BarButton} from "../BarButton";
 import styles from "./ControlCenter.module.sass";
+import {Switch} from "@headlessui/react";
 
 export const ControlCenter: React.FC = () => {
+  const width = '16px';
+  const height = '7px';
+
   return (
     <BarButton>
       <BarButton.Button>
         <div>
-          <div className={styles.toggleOff}/>
-          <div className={styles.toggleOn}/>
+          <Switch
+            className="border border-dark-content dark:border-white relative flex items-center rounded-full"
+            style={{height,width,marginBottom:'1px'}}
+            checked={true}
+            onChange={() => {}}>
+            <span
+              style={{left:'1px'}}
+              className={`bg-dark-content dark:bg-white inline-block w-1 h-1 absolute transform bg-white rounded-full`}
+            />
+          </Switch>
+          <Switch
+            className="bg-dark-content border border-dark-content dark:border-white dark:bg-white w-4 relative flex items-center rounded-full"
+            style={{width,height}}
+            checked={true}
+            onChange={() => {}}>
+            <span
+              style={{right:'1px'}}
+              className={`dark:bg-dark-content bg-zinc-100/90 inline-block w-1 h-1 absolute transform bg-white rounded-full`}
+            />
+          </Switch>
         </div>
       </BarButton.Button>
     </BarButton>
