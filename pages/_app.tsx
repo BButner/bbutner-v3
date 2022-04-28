5725536100d97e0273a5e0afa20e4a93debad3c5
BButner <butner.beau@gmail.com>
2022-04-27 22:08:30 -0500

    Fix theme when no local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,6 +25,8 @@ function MyApp({Component, pageProps}: AppProps) {
       }
     }
 
+    updateCurrentTheme();
+
 //    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 //    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
 //      // document.documentElement.classList.add('dark');
